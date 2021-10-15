b489e99e3e161ade3718ae9298b23427d0143994
in2dam <in2dam@elorrieta.int>
2021-10-15 09:55:52 +0200

    Lehenengo sprinta

---
+++ src/FitxategiDOM.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
+public class FitxategiDOM {
+	public static void main(String[] args) {
+
+		try {
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			MiHandler miHandler = new MiHandler();
+			SAXParser parser = factory.newSAXParser();
+			parser.parse("Liburuak.xml", miHandler);
+			ArrayList<Liburua> Libros = miHandler.obtenerLiburu();
+
+			for (Liburua lib : Libros) {
+				System.out.println("------------------------------------------------------");
+				System.out.println("Izenburua: " + lib.getIzenburua());
+				System.out.println("Argitaletxea: " + lib.getArgitaletxea());
+				System.out.println("Orrialdeak: " + lib.getOrrialdeak());
+				System.out.println("Altuera: " + lib.getAltuera());
+				System.out.println("Oharrak: " + lib.getOharrak());
+				System.out.println("ISBN: " + lib.getISBN());
+				System.out.println("Gaiak: " + lib.getGaiak());
+			}
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/MiHandler.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class MiHandler extends DefaultHandler{
+	
+	boolean izenburua = false;
+	boolean argitaletxea = false;
+	boolean orrialdeak = false;
+	boolean altuera = false;
+	boolean oharrak = false;
+	boolean isbn = false;
+	boolean gaiak = false;
+	private ArrayList<Liburua> liburu = new ArrayList<Liburua>();
+	Liburua milibro = new Liburua();
+	
+	 @Override
+	    public void startElement(String uri, String localName, String Name, Attributes attributos) throws SAXException {
+	       	if(Name.equals("Izenburua"))
+	       		izenburua = true;
+	    	if(Name.equals("Argitaletxea"))
+	    		argitaletxea = true;
+	    	if(Name.equals("Orrialdeak"))
+	    		orrialdeak = true;
+	    	if(Name.equals("Altuera"))
+	    		altuera = true;
+	    	if(Name.equals("Oharrak"))
+	    		oharrak = true;
+	    	if(Name.equals("ISBN"))
+	    		isbn = true;
+	    	if(Name.equals("Gaiak"))
+	    		gaiak = true;
+		 
+	    }
+	 
+	 
+	  public ArrayList<Liburua> obtenerLiburu(){
+		  return liburu;
+	  }
+	 
+	    @Override
+	    public void characters(char[] ch, int start, int length) throws SAXException {
+	    
+	        if(izenburua)
+	        {
+	        	milibro.setIzenburua(new String(ch, start, length));
+	        	izenburua = false;
+	        }
+	        
+	        if(argitaletxea)
+	        {
+	        	milibro.setArgitaletxea(new String(ch, start, length));
+	        	argitaletxea = false;
+	        }
+	        
+	        if(orrialdeak)
+	        {
+	        	milibro.setOrrialdeak(new String(ch, start, length));
+	        	orrialdeak = false;
+	        }
+	        if(altuera)
+	        {
+	        	milibro.setAltuera(new String(ch, start, length));
+	        	altuera = false;
+	        }
+	        if(oharrak)
+	        {
+	        	milibro.setOharrak(new String(ch, start, length));
+	        	oharrak = false;
+	        }
+	        if(isbn)
+	        {
+	        	milibro.setISBN(new String(ch, start, length));
+	        	isbn = false;
+	        }
+	        if(gaiak)
+	        {
+	        	milibro.setGaiak(new String(ch, start, length));
+	        	gaiak = false;
+	        }
+	        
+	        	
+	    }
+	 
+	    @Override
+	    public void endElement(String uri, String localName, String qName) throws SAXException {
+	    	if(qName.equals("Liburua"))
+	    	{
+		    	liburu.add(milibro);
+		    	milibro = new Liburua();
+	    	}	
+	       
+	    }
+}
